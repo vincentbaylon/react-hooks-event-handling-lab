29f209ffb18d819c576670c494ab0cef942c0d69
vincentbaylon <vincentbaylon@gmail.com>
2021-07-22 09:41:31 -0700

    commit tree 95d1d0e060ebd2cc6f007c13262a7adeee4ec70a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function handleFocus(event) {
+    console.log('Good!')
+}
+
+function handleBlur(event) {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return (
+        <>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+function onChangeEvent(event) {
+    console.log('Entering password...')
+}
+
+function Keypad() {
+    return (
+        <>
+        <input type="password" onChange={onChangeEvent}></input>
+        </>
+    )
+}
+
+export default Keypad
